c10d614819cbee77b648f1f124e1675578bc5c2e
starving-array <gowork.archis@gmail.com>
2022-09-27 13:53:38 +0530

    spring boot project layout

---
+++ BackendService/src/main/java/com/reservation/BackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendServiceApplication.class, args);
+	}
+
+}
---
+++ BackendService/src/test/java/com/reservation/BackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
