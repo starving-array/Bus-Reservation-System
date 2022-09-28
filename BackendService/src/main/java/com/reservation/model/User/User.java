d9afafa8c5b88cc67eaa664270469ae2cd9df7e4
Aman Kumar <savverma98@gmail.com>
2022-09-28 12:50:14 +0530

    User Model add

---
+++ BackendService/src/main/java/com/reservation/model/User/User.java
@@ -0,0 +1,37 @@
+package com.reservation.model.User;
+
+import java.util.List;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+
+import com.reservation.model.Reservation;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int userLoginId;
+	private String UserName;
+	private String password;
+	private String firstName;
+	private String LastName;
+	private long contact;
+	private String email;
+
+//	private Wallet wallet;
+	private List<Reservation> reservation;
+	
+}
