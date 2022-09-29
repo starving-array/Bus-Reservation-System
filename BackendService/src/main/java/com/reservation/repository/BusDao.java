63e6496a56ba0c95fb244b67b0e515b55f43e4e6
Shubhambhadouria <shubhambhadouria07@gmail.com>
2022-09-29 22:23:30 +0530

    BusDao Repository Linked

--- BackendService/src/main/java/com/reservation/repository/BusDao.java
+++ BackendService/src/main/java/com/reservation/repository/BusDao.java
@@ -1,5 +1,11 @@
 package com.reservation.repository;
 
-public interface BusDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.reservation.model.Bus.Bus;
+
+@Repository
+public interface BusDao extends JpaRepository<Bus, Integer> {
 
 }
