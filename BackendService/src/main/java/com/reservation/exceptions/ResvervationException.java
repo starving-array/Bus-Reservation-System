85c97ad1fefdbe24a746da5255561967cf17ed41
Shashank Pandey <101421706+SHASHANKpandey-ncr@users.noreply.github.com>
2022-09-29 12:35:53 +0530

    reservationException added

---
+++ BackendService/src/main/java/com/reservation/exceptions/ResvervationException.java
@@ -0,0 +1,15 @@
+package com.reservation.exceptions;
+
+public class ResvervationException extends Exception{
+
+	public ResvervationException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public ResvervationException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+	
+}
