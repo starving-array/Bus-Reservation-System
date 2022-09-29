58041aad7b9fab1791c79db55d52103470cd2ab6
starving-array <gowork.archis@gmail.com>
2022-09-29 22:42:31 +0530

    route repository linked with @service

--- BackendService/src/main/java/com/reservation/service/RouteService/RouteServiceImpl.java
+++ BackendService/src/main/java/com/reservation/service/RouteService/RouteServiceImpl.java
@@ -4,12 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.reservation.exceptions.RouteException;
 import com.reservation.model.Route.Route;
 import com.reservation.repository.RouteDao;
 
-
+@Service
 public class RouteServiceImpl implements RouteService {
 
 	@Autowired
@@ -38,6 +39,7 @@ public class RouteServiceImpl implements RouteService {
 		if (routeExistence.isPresent()) {
 			// if route already active throw exception
 			// else set statues to its active status
+			return routeExistence.get();
 		} else {
 			throw new RouteException("Route doesnot exitsts");
 		}
@@ -45,7 +47,7 @@ public class RouteServiceImpl implements RouteService {
 
 	@Override
 	public Route viewRoute(Integer routeId) throws RouteException {
-		Optional<Route> routeExistence = routeDao.findById(route.getRouteId());
+		Optional<Route> routeExistence = routeDao.findById(routeId);
 		if (routeExistence.isPresent()) {
 			return routeExistence.get();
 		} else {
