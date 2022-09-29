c7fe70b35c2b53c884b1335df07d6966e71cc3ad
Shubhambhadouria <shubhambhadouria07@gmail.com>
2022-09-29 11:05:08 +0530

    BusService Implemenatation added

--- BackendService/src/main/java/com/reservation/service/BusService/BusService.java
+++ BackendService/src/main/java/com/reservation/service/BusService/BusService.java
@@ -1,5 +1,17 @@
 package com.reservation.service.BusService;
 
+import java.util.List;
+
+import com.reservation.exceptions.BusException;
+import com.reservation.model.Bus.Bus;
+
 public interface BusService {
 
+	public Bus addBus(Bus bus) throws BusException;
+	public Bus updateBus(Bus bus) throws BusException;
+	public Bus deleteBus(int busId) throws BusException;
+	public Bus viewBus(int busId) throws BusException;
+	public String busTypeAc(int busId) throws BusException;
+	public List<Bus> viewAllBus() throws BusException;
+	
 }
