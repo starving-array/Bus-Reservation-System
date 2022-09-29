a93e89edc4d1182bae304364629275553927bf69
Aman Kumar <savverma98@gmail.com>
2022-09-29 12:15:29 +0530

    User Service and userService Implemnetation add In User.Model

--- BackendService/src/main/java/com/reservation/service/UserService/UserService.java
+++ BackendService/src/main/java/com/reservation/service/UserService/UserService.java
@@ -1,5 +1,19 @@
 package com.reservation.service.UserService;
 
+import java.util.List;
+
+import com.reservation.model.User.User;
+
 public interface UserService {
+
+	public User addUser(User user) throws UserException;
+
+	public User updateUser(User user) throws UserException;
+
+	public User deleteUser(Integer userId) throws UserException;
+
+	public User viewUser(Integer userId) throws UserException;
+
+	public List<User> viewAllUser() throws UserException;
 
 }
