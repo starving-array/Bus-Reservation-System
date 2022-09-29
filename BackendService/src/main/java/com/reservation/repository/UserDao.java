8a703594537f63a2b2cb1baa56de2d74942a17a5
Aman Kumar <savverma98@gmail.com>
2022-09-29 22:18:04 +0530

    link repository UserDao

--- BackendService/src/main/java/com/reservation/repository/UserDao.java
+++ BackendService/src/main/java/com/reservation/repository/UserDao.java
@@ -1,5 +1,11 @@
 package com.reservation.repository;
 
-public interface UserDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.reservation.model.User.User;
+
+@Repository
+public interface UserDao extends JpaRepository<User, Integer> {
 
 }
