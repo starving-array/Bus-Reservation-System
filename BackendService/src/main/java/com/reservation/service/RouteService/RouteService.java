9b46956f416f427a2d1f619010904f6254bc94b7
starving-array <gowork.archis@gmail.com>
2022-09-29 10:56:45 +0530

    Route Service and Implementation

--- BackendService/src/main/java/com/reservation/service/RouteService/RouteService.java
+++ BackendService/src/main/java/com/reservation/service/RouteService/RouteService.java
@@ -1,5 +1,19 @@
 package com.reservation.service.RouteService;
 
+import com.reservation.model.Route.Route;
+
+import java.util.*;
+
 public interface RouteService {
+	
+	public Route addRoute(Route route) throws RouteException;
+	
+	public Route updateRoute(Route route) throws RouteException;
+	
+	public Route routeActive(Route route, Boolean status) throws RouteException;
+	
+	public Route viewRoute(Integer routeId) throws RouteException;
+	
+	public List<Route> viewAllRoute(Route route) throws RouteException;
 
 }
--- BackendService/src/main/java/com/reservation/service/RouteService/RouteServiceImpl.java
+++ BackendService/src/main/java/com/reservation/service/RouteService/RouteServiceImpl.java
@@ -1,5 +1,64 @@
 package com.reservation.service.RouteService;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.reservation.model.Route.Route;
+
+
 public class RouteServiceImpl implements RouteService {
+
+	@Autowired
+	private RouteDao routeDao;
+
+	@Override
+	public Route addRoute(Route route) throws RouteException {
+		// TODO Auto-generated method stub
+		Route routenew = routeDao.save(route); // single data exist in the database; throw warning if already exist
+		return routenew;
+	}
+
+	@Override
+	public Route updateRoute(Route route) throws RouteException {
+		Optional<Route> routeExistence = routeDao.findById(route.getRouteId());
+		if (routeExistence.isPresent()) {
+			return routeDao.save(route);
+		} else {
+			throw new RouteException("Route doesnot exitsts");
+		}
+	}
+
+	@Override
+	public Route routeActive(Route route, Boolean status) throws RouteException {
+		Optional<Route> routeExistence = routeDao.findById(route.getRouteId());
+		if (routeExistence.isPresent()) {
+			// if route already active throw exception
+			// else set statues to its active status
+		} else {
+			throw new RouteException("Route doesnot exitsts");
+		}
+	}
+
+	@Override
+	public Route viewRoute(Integer routeId) throws RouteException {
+		Optional<Route> routeExistence = routeDao.findById(route.getRouteId());
+		if (routeExistence.isPresent()) {
+			return routeExistence.get();
+		} else {
+			throw new RouteException("Route doesnot exitsts");
+		}
+	}
+
+	@Override
+	public List<Route> viewAllRoute() throws RouteException {
+		List<Route> listOfRoute = routeDao.findAll();
+		if(listOfRoute.size()>0) {
+			return listOfRoute;
+		}else {
+			throw new RouteException("No route found in the database");
+		}
+	}
 
 }
