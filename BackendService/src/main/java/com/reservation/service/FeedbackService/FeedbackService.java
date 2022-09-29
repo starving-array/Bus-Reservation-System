13d2a18f37ce34528320fd3f52f5538599390ab7
Gaurav Singh <gaurav7185singh@gmail.com>
2022-09-29 11:32:09 +0530

    feedback service and impl added

--- BackendService/src/main/java/com/reservation/service/FeedbackService/FeedbackService.java
+++ BackendService/src/main/java/com/reservation/service/FeedbackService/FeedbackService.java
@@ -1,5 +1,16 @@
 package com.reservation.service.FeedbackService;
 
+import java.util.List;
+
+import com.reservation.model.Feedback.Feedback;
+
 public interface FeedbackService {
+	public Feedback addFeedBack(Feedback feedback) throws FeedBackException;
+
+	public Feedback updateFeedBack(Feedback feedback) throws FeedBackException;
+
+	public Feedback viewFeedBack(int feedbackid) throws FeedBackException;
+
+	public List<Feedback> viewAllFeedBack() throws FeedBackException;
 
 }
