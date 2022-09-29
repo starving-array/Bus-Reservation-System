31bbb957f51a7aa72500067c2ddf780f93f1f085
Gaurav Singh <gaurav7185singh@gmail.com>
2022-09-29 11:47:57 +0530

    feedbackException added

---
+++ BackendService/src/main/java/com/reservation/exceptions/FeedBackException.java
@@ -0,0 +1,15 @@
+package com.reservation.exceptions;
+
+public class FeedBackException extends Exception {
+	
+	public FeedBackException(String msg) {
+		
+		super(msg);
+		
+	}
+	 
+	public FeedBackException() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
